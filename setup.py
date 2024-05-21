5a682f880447d8e42d8330ebe91c6fad6e13b70f
Matt Newsted <mnewsted@illinoislegalaid.org>
2024-05-21 11:54:11 -0500

    instructions-added-other-areas-in-progress

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.Appearance',
-      version='1.2.18',
+      version='1.2.19',
       description=('Appearance'),
       long_description='# docassemble.Appearance\r\n\r\nAppearance\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -54,7 +54,7 @@ setup(name='docassemble.Appearance',
       url='https://www.illinoislegalaid.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.23.0', 'docassemble.ILAO'],
+      install_requires=['docassemble.AssemblyLine>=2.28.1', 'docassemble.ILAO'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/Appearance/', package='docassemble.Appearance'),
      )
